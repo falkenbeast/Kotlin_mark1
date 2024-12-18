63ef97fbba06332ba953a3abfbba500540293e58
falkenbeast <arshayush21@gmail.com>
2024-12-18 16:37:37 +0530

    first commit

---
+++ variables.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var stringvariable : String = "my first string"
+     println(stringvariable)
+    
+
+    val myfixedvalue : Int = 6
+    println(myfixedvalue)
+
+    var myinferredvariabletype = "no datatype specified"
+    println(myinferredvariabletype) //it automatically takes as Stirng
+
+
+    var myintvariable : Int = 323
+    println(myintvariable)
+}
